c601eb105bd31e2ade5955bc9ec49de6234cf33f
juyun <juyun571@gmail.com>
2023-09-29 14:42:11 +0900

    first

---
+++ pybo/views/question_views.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, render_template,request, url_for
+from pybo.forms import QuestionForm
+from werkzeug.utils import redirect
+from pybo.models import Question
+from datetime import datetime
+from .. import db
+from ..models import Question
+from ..forms import QuestionForm
+
+bp = Blueprint('question', __name__, url_prefix='/question')
+
+
+@bp.route('/list/')
+def _list():
+    question_list = Question.query.order_by(Question.create_date.desc())
+    return render_template('question/question_list.html', question_list=question_list)
+
+
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question = Question.query.get_or_404(question_id)
+    return render_template('question/question_detail.html', question=question) 
+
+@bp.route('/create/', methods=('GET', 'POST'))
+def create():
+    form = QuestionForm()
+    if request.method =='POST' and form.validate_on_submit():
+        question = Question(subject=form.subject.data, content=form.content.data, price=form.price.data, create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    return render_template('question/question_form.html', form=form)
