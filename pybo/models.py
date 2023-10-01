5dd3b396be7d722a8d8e2e330c8658013bcb5633
juyun <juyun571@gmail.com>
2023-10-01 23:32:48 +0900

    주문목록표시

--- pybo/models.py
+++ pybo/models.py
@@ -7,12 +7,6 @@ class Question(db.Model):
     price = db.Column(db.String(200), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
     
-class Answer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
-    question = db.relationship('Question', backref=db.backref('answer_set'))
-    content = db.Column(db.Text(), nullable=False)
-    create_date = db.Column(db.DateTime(), nullable=False)
     
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,3 +20,28 @@ class CartItem(db.Model):
     price = db.Column(db.Integer, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     question = db.relationship('Question', backref='cart_items')
+    
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    create_date = db.Column(db.DateTime(), nullable=False)
+    total_price = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String(20), nullable=False)  # 주문 상태 (예: "대기 중", "완료됨" 등)
+    
+
+class OrderedItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    
+    order = db.relationship('Order', backref='ordered_items')
+    question = db.relationship('Question', backref='ordered_items')
+    
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
+    question = db.relationship('Question', backref=db.backref('answer_set'))
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+    
--- pybo/views/order_views.py
+++ pybo/views/order_views.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for,flash
 from flask import Blueprint
-from ..models import Question,CartItem,db
+from ..models import Question,CartItem,db,Order, OrderedItem
+from datetime import datetime
 
 bp = Blueprint('order', __name__, url_prefix='/order')
 
@@ -35,3 +36,37 @@ def remove_item(cart_item_id):
     db.session.commit()
    
     return redirect('/order/cart')
+
+
+@bp.route('/create_order', methods=['POST'])
+def create_order():
+    # 새로운 주문 생성
+    new_order = Order(create_date=datetime.now(), total_price=0, status="대기 중")
+    db.session.add(new_order)
+    db.session.commit()
+
+    # 장바구니에서 선택된 음료를 주문 목록에 추가
+    cart_items = CartItem.query.all()  # 장바구니에 담긴 음료 조회
+    for cart_item in cart_items:
+        ordered_item = OrderedItem(
+            order=new_order,
+            question=cart_item.question,
+            price=cart_item.price,
+            quantity=cart_item.quantity
+        )
+        new_order.total_price += ordered_item.price * ordered_item.quantity
+        db.session.add(ordered_item)
+    
+    # 장바구니 비우기
+    CartItem.query.delete()
+    db.session.commit()
+
+    flash('주문이 생성되었습니다.', 'success')
+    return redirect(url_for('order.order_list'))
+
+@bp.route('/order_list')
+def order_list():
+    # 주문 목록 조회
+    orders = Order.query.all()
+   
+    return render_template('question/order_list.html', orders=orders)
