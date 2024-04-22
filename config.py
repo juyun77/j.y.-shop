0bd1ce330e28ba895abc6679940b999d5ed31640
juyun <juyun571@gmail.com>
2024-04-22 16:53:25 +0900

    database url 변경

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+
+
+##sqlite
 # import os
 #
 # BASE_DIR = os.path.dirname(__file__)
@@ -5,15 +8,29 @@
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'shop.db'))
 # SQLALCHEMY_TRACK_MODIFICATIONS = False
 #
+
+##mysql
+# import os
+#
+# class Config(object):
+#
+#
+#     # 데이터베이스 설정
+#     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1111@localhost/shop'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#
+#     # 추가적인 설정이 필요할 경우 이곳에 추가
+#
+#     SECRET_KEY = "dev"
+
+##aws rds
 import os
 
 class Config(object):
 
 
     # 데이터베이스 설정
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1111@localhost/shop'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:It12345!@database-1.cluster-ro-cmg5rcfhykpl.ap-northeast-2.rds.amazonaws.com/shop'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # 추가적인 설정이 필요할 경우 이곳에 추가
 
     SECRET_KEY = "dev"
