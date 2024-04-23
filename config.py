03c480f4abb46a00e3d3b5b1f15eec828d970e3f
juyun <juyun571@gmail.com>
2024-04-23 14:02:03 +0900

    db url 변경

--- config.py
+++ config.py
@@ -9,28 +9,28 @@
 # SQLALCHEMY_TRACK_MODIFICATIONS = False
 #
 
-##mysql
-import os
-
-class Config(object):
-
-
-    # 데이터베이스 설정
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1111@192.168.0.80/shop'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # 추가적인 설정이 필요할 경우 이곳에 추가
-
-    SECRET_KEY = "dev"
-
-##aws rds
+# ##mysql
 # import os
 #
 # class Config(object):
 #
 #
 #     # 데이터베이스 설정
-#     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:It12345!@database-1.cluster-ro-cmg5rcfhykpl.ap-northeast-2.rds.amazonaws.com/shop'
+#     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1111@192.168.0.80/shop'
 #     SQLALCHEMY_TRACK_MODIFICATIONS = False
 #
+#     # 추가적인 설정이 필요할 경우 이곳에 추가
+#
 #     SECRET_KEY = "dev"
+
+############################aws rds
+import os
+
+class Config(object):
+
+
+    # 데이터베이스 설정
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:It12345!@aurora-db.cluster-cmg5rcfhykpl.ap-northeast-2.rds.amazonaws.com/shop'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = "dev"
